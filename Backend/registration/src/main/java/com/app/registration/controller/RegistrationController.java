af16876f832bd36ecb484fd6abf16b3a3a44b6d2
chippevijay <49764935+chippevijay@users.noreply.github.com>
2020-08-31 20:25:36 +0530

    backend

---
+++ Backend/registration/src/main/java/com/app/registration/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+package com.app.registration.controller;
+
+import com.app.registration.model.User;
+import com.app.registration.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+     @Autowired
+     private RegistrationService service;
+     @PostMapping("/registeruser")
+     public User registerUser(@RequestBody User user) throws Exception {
+         String tempEmailId = user.getEmailId();
+         if(tempEmailId != null && !"".equals(tempEmailId)){
+               User userObj =service.fetchUserByEmailId(tempEmailId);
+               if(userObj != null){
+                   throw new Exception("User  with"+tempEmailId+" is already exists!");
+               }
+         }
+         User userObj = null;
+         userObj=service.saveUser(user);
+         return userObj;
+     }
+     @PostMapping("/login")
+     public User loginUser(@RequestBody User user) throws Exception {
+         String tempEmailId = user.getEmailId();
+         String tempPass = user.getPassword();
+         User userobj =null;
+         if(tempEmailId!=null && tempPass!=null){
+              userobj = service.fetchUserByEmailIdAndPassword(tempEmailId,tempPass);
+         }
+         if(userobj == null){
+             throw new Exception("User Doesn't exist..");
+         }
+         return userobj;
+     }
+}
